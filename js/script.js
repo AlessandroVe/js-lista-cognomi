1c62a8c92ab12327bbdc826f3b13e02dc3ab4608
AlessandroVe <aveneziano536@gmail.com>
2021-06-12 09:21:39 +0200

    .

--- js/script.js
+++ js/script.js
@@ -3,6 +3,33 @@
 //2. inseriscilo in un array dove sono presenti già altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
 //3. stampa la lista ordinata alfabeticamente
 //4. scrivi anche la posizione “umana” (partendo da 1) della lista in cui il nuovo utente si trova
+
+var ul = document.getElementsByClassName("lista")[0];
+
+//1. chiedi all’utente il cognome
 var surname = prompt("inserisci qui il tuo cognome");
-regedit=["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
-regedit.push(surname)
+
+/* inseriscilo in un array dove sono presenti già altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’  */
+var regedit=["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
+
+regedit.push(surname);
+//3. stampa la lista ordinata alfabeticamente
+function confrontaS(a,b) {
+    var minA = a.toLowerCase();
+    var minB = b.toLowerCase();
+    if (minA < minB) { return -1; }
+        else {
+           if (minA > minB) {
+                return 1;
+            }else {
+                 return 0; }
+            }
+    }
+console.log(regedit.sort(confrontaS));
+
+//4. scrivi anche la posizione “umana” (partendo da 1) della lista in cui il nuovo utente si trova
+
+for(i=0; i < regedit.length; i++){
+    var indice = i + 1;
+    ul.innerHTML += "<li>" + regedit[i] + " " + indice + "</li>";
+}
